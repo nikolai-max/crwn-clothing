bd5c647b12c559243fc20f85f4e5c647237e05d1
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-04-28 16:27:02 +0200

    Added styling for meny items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './hompage.style.scss'
+import Directory from '../../components/directory/directory.component'
+
+const Homepage = () => (
+  <div className='homepage'>
+    <Directory></Directory>
+  </div>
+)
+
+export default Homepage
